2a8826f1287fbdeebdd14d700a34cd3f6f7dba92
Jason Sinn <jrs341@me.com>
2016-12-02 16:48:51 -0600

    changed files names login page is now index and previous index is now app

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
+// var BreweryDb = require('brewerydb-node');
+// var brewdb = new BreweryDb('your-key-here');
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'))
+
+// app.use(bodyParser.json());
+
+// app.get('/api/beers', function(req, res) {
+
+// });
 
 app.listen(3000);
